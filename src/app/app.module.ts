e54136e3cd21ffa6b8a6b050d7a3165836a1dbbe
ShristiPokhrel <shristipok2000@gmail.com>
2022-11-30 15:38:50 +0545

    about added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LearnComponent } from './learn/learn.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LearnComponent
+    LearnComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
